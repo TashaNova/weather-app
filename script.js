ccbb05925730090dedc1f6f1cd8746cdc89bb1c0
TashaNova <107804773+TashaNova@users.noreply.github.com>
2022-08-28 17:10:30 +0300

    Improved api and weather details

--- script.js
+++ script.js
@@ -19,18 +19,40 @@ function showCityTemp(response) {
   console.log(response);
   userTemp.innerHTML = Math.round(response.data.main.temp);
   newCity.innerHTML = userCity.value;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  let pressure = document.querySelector("#pressure");
-  pressure.innerHTML = `Pressure: ${response.data.main.pressure}`;
+  let windSpeadElement = document.querySelector("#windSpead");
+  let descriptionElement = document.querySelector("#description");
+  let iconElement = document.querySelector("#icon");
+
+  windSpeadElement.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 //Date
+
 let now = new Date();
 let time = document.querySelector("#time-string");
 let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
 let minutes = now.getMinutes();
-let days = ["Sun", "Mon", "Tue", "Wed", "Tue", "Fr", "Sat"];
-let day = days[1];
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
 
 time.innerHTML = `${day} ${hour}:${minutes}`;
