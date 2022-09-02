805a3b8d8b209baccb078b2175d2e4be2aa9b077
TashaNova <107804773+TashaNova@users.noreply.github.com>
2022-09-02 17:01:07 +0300

    added forecast base

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //User temp
+function searchCity(event) {
+  event.preventDefault();
+  let apiKey = "8c49740cd3e1e284d60cee5be68d2a24";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showCityTemp);
+}
 
 let citySearch = document.querySelector("form");
 let userCity = document.querySelector("#searchMenu");
@@ -6,14 +13,6 @@ let newCity = document.querySelector("#cityName");
 let userTemp = document.querySelector("#current-temperature");
 
 citySearch.addEventListener("click", searchCity);
-
-function searchCity(event) {
-  event.preventDefault();
-  let apiKey = "c08cfe93e8eb9d83c3e646f295df45f5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userCity.value}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(showCityTemp);
-}
 
 function showCityTemp(response) {
   console.log(response);
@@ -32,8 +31,34 @@ function showCityTemp(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-//Date
+function getForecast(coordinates) {
+  console.log(coordinates);
+}
 
+function displayForcast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forcastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forcastHTML += `<div class="col-3">
+              <div class="forcast-days">
+                Fr
+              <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="35PX">
+              </div> 
+              <div class="forecast-temperature">
+               <span>🌝 +18</span> 
+               <span>🌚 +12</span>
+               </div>
+		       </div>`;
+  });
+  forcastHTML += `</div>`;
+  forecastElement.innerHTML = forcastHTML;
+}
+displayForcast();
+getForecast(response.data.coord);
+
+//Date
+function formatDate (timestamp)
 let now = new Date();
 let time = document.querySelector("#time-string");
 let hour = now.getHours();
